6e42efc87650810a093193bde5a67b995819fe23
moataz-armash <mo3taz314@gmail.com>
2024-04-18 00:43:11 +0300

    build the 1st version

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import Header from "./components/Header";
+import UserInput from "./components/UserInput";
+import Result from "./components/Result";
+import { useState } from "react";
+
+function App() {
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10,
+  });
+
+  const inputIsValid = userInput.duration >= 1;
+  function handleChange(inputIdentifier, newValue) {
+    setUserInput((prevUserInput) => {
+      return {
+        ...prevUserInput,
+        [inputIdentifier]: +newValue,
+      };
+    });
+  }
+  return (
+    <>
+      <Header />
+      <UserInput onChange={handleChange} userInput={userInput} />
+      {!inputIsValid && (
+        <p className="center">Please enter a duration greater than zero.</p>
+      )}
+      {inputIsValid && <Result input={userInput} />}
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import logo from "../assets/investment-calculator-logo.png";
+function Header() {
+  return (
+    <header id="header">
+      <img src={logo} alt="investment-calculator-logo" />
+      <h1>Investment Calculater</h1>
+    </header>
+  );
+}
+
+export default Header;
